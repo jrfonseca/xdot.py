53c9a41299ddd22c1d4f4cd20d383bcdef71c45a
Peter Hill <zed.three@gmail.com>
2016-07-02 16:34:17 +0100

    Move lookup_color into ui.colours
    Now all dependencies on GUI library are in xdot.ui

--- xdot/ui/colours.py
+++ xdot/ui/colours.py
@@ -303,3 +303,46 @@ brewer_colors = {
     'ylorrd7': [(255, 255, 178), (254, 217, 118), (254, 178, 76), (253, 141, 60), (252, 78, 42), (227, 26, 28), (177, 0, 38)],
     'ylorrd8': [(255, 255, 204), (255, 237, 160), (254, 217, 118), (254, 178, 76), (253, 141, 60), (252, 78, 42), (227, 26, 28), (177, 0, 38)],
 }
+
+
+def lookup_color(c):
+    """Return RGBA values of colour c
+
+    c should be either an X11 colour or a brewer colour set and index
+    e.g. "navajowhite", "greens3/2"
+
+    """
+    import sys
+    import gi
+    gi.require_version('Gtk', '3.0')
+    gi.require_version('PangoCairo', '1.0')
+
+    from gi.repository import Gdk
+
+    try:
+        color = Gdk.color_parse(c)
+    except ValueError:
+        pass
+    else:
+        s = 1.0/65535.0
+        r = color.red*s
+        g = color.green*s
+        b = color.blue*s
+        a = 1.0
+        return r, g, b, a
+
+    try:
+        dummy, scheme, index = c.split('/')
+        r, g, b = brewer_colors[scheme][int(index)]
+    except (ValueError, KeyError):
+        pass
+    else:
+        s = 1.0/255.0
+        r = r*s
+        g = g*s
+        b = b*s
+        a = 1.0
+        return r, g, b, a
+
+    sys.stderr.write("warning: unknown color '%s'\n" % c)
+    return None
