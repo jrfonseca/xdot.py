696af52a723f83a84fee802ea7b9a099fc816fa0
Jose Fonseca <jose.r.fonseca@gmail.com>
2016-11-15 16:11:41 +0000

    Run tests in parallel with multiprocessing.
    Tests run much faster like that.
    
    Logging should be improved, but that is left to another opportunity.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2015 Jose Fonseca
+# Copyright 2015-2016 Jose Fonseca
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published
@@ -20,104 +20,125 @@
 import sys
 import os.path
 import traceback
-
-import gi
-gi.require_version('Gtk', '3.0')
-
-from gi.repository import Gtk
-from gi.repository import Gdk
-
-from xdot import DotWidget, DotWindow
+import multiprocessing
 
 
-class TestDotWidget(DotWidget):
+def test(arg):
+    sys.stdout.write(arg + '\n')
+    sys.stdout.flush()
 
-    def __init__(self, name):
-        DotWidget.__init__(self)
-        self.name = name
 
-    def on_draw(self, widget, cr):
-        DotWidget.on_draw(self, widget, cr)
+    import gi
+    gi.require_version('Gtk', '3.0')
 
-        if True:
-            # Cairo screenshot
+    from gi.repository import Gtk
+    from gi.repository import Gdk
 
-            import cairo
+    from xdot import DotWidget, DotWindow
 
-            # Scale to give 96 dpi instead of 72 dpi
-            dpi = 96.0
-            scale = dpi/72.0
-            w = int(self.graph.width*scale)
-            h = int(self.graph.height*scale)
 
-            CAIRO_XMAX = 32767
-            CAIRO_YMAX = 32767
-            if w >= CAIRO_XMAX:
-                w = CAIRO_XMAX
-                scale = w/self.graph.width
-                h = int(self.graph.height*scale)
-            if h >= CAIRO_YMAX:
-                h = CAIRO_YMAX
-                scale = h/self.graph.height
+    class TestDotWidget(DotWidget):
+
+        def __init__(self, name):
+            DotWidget.__init__(self)
+            self.name = name
+
+        def on_draw(self, widget, cr):
+            DotWidget.on_draw(self, widget, cr)
+
+            if True:
+                # Cairo screenshot
+
+                import cairo
+
+                # Scale to give 96 dpi instead of 72 dpi
+                dpi = 96.0
+                scale = dpi/72.0
                 w = int(self.graph.width*scale)
+                h = int(self.graph.height*scale)
 
-            assert w <= CAIRO_XMAX
-            assert h <= CAIRO_YMAX
+                CAIRO_XMAX = 32767
+                CAIRO_YMAX = 32767
+                if w >= CAIRO_XMAX:
+                    w = CAIRO_XMAX
+                    scale = w/self.graph.width
+                    h = int(self.graph.height*scale)
+                if h >= CAIRO_YMAX:
+                    h = CAIRO_YMAX
+                    scale = h/self.graph.height
+                    w = int(self.graph.width*scale)
 
-            surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
+                assert w <= CAIRO_XMAX
+                assert h <= CAIRO_YMAX
 
-            cr = cairo.Context(surface)
+                surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
 
-            cr.set_source_rgba(1.0, 1.0, 1.0, 1.0)
-            cr.paint()
+                cr = cairo.Context(surface)
 
-            cr.scale(scale, scale)
+                cr.set_source_rgba(1.0, 1.0, 1.0, 1.0)
+                cr.paint()
 
-            self.graph.draw(cr, highlight_items=self.highlight)
+                cr.scale(scale, scale)
 
-            surface.write_to_png(self.name + '.png')
+                self.graph.draw(cr, highlight_items=self.highlight)
 
-        if False:
-            # GTK 3 screenshot
+                surface.write_to_png(self.name + '.png')
 
-            window = self.get_window()
+            if False:
+                # GTK 3 screenshot
 
-            w = window.get_width()
-            h = window.get_height()
+                window = self.get_window()
 
-            pixbuf = Gdk.pixbuf_get_from_window(window, 0, 0, w, h)
+                w = window.get_width()
+                h = window.get_height()
 
-            pixbuf.savev(self.name + '.png', 'png', (), ())
+                pixbuf = Gdk.pixbuf_get_from_window(window, 0, 0, w, h)
 
-        Gtk.main_quit()
+                pixbuf.savev(self.name + '.png', 'png', (), ())
 
-    def error_dialog(self, message):
-        sys.stderr.write(message)
-        sys.stderr.write("\n")
+            Gtk.main_quit()
+
+        def error_dialog(self, message):
+            sys.stderr.write(message)
+            sys.stderr.write("\n")
+
+
+    result = True
+
+    name, ext = os.path.splitext(os.path.basename(arg))
+    widget = TestDotWidget(name)
+    window = DotWindow(widget)
+    window.connect('delete-event', Gtk.main_quit)
+    try:
+        try:
+            dotcode = open(arg, 'rb').read()
+            window.set_dotcode(dotcode)
+        except:
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            result = False
+        else:
+            window.show()
+            Gtk.main()
+    finally:
+        window.destroy()
+
+    return result
 
 
 def main():
-    status = 0
-    for arg in sys.argv[1:]:
-        sys.stdout.write(arg + '\n')
-        sys.stdout.flush()
-        name, ext = os.path.splitext(os.path.basename(arg))
-        widget = TestDotWidget(name)
-        window = DotWindow(widget)
-        window.connect('delete-event', Gtk.main_quit)
-        try:
-            try:
-                dotcode = open(arg, 'rb').read()
-                window.set_dotcode(dotcode)
-            except:
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                status = 1
-            else:
-                window.show()
-                Gtk.main()
-        finally:
-            window.destroy()
+    args = sys.argv[1:]
+
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    results = pool.map(test, args)
+
+    # Exit with status 1 if any failed
+    try:
+        results.index(False)
+    except ValueError:
+        status = 0
+    else:
+        status = 1
     sys.exit(status)
 
 
