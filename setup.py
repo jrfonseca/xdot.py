3f866766be44adbe3634de95cd0a031e8a863c91
Balló György <ballogyor@gmail.com>
2018-01-26 23:05:00 +0100

    Don't check for graphviz python module
    It's not used by xdot. Fixes #51.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         """,
     license="LGPL",
 
-    install_requires=['graphviz'],
     packages=['xdot', 'xdot/dot', 'xdot/ui'],
     entry_points=dict(gui_scripts=['xdot=xdot.__main__:main']),
 
