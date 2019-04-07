6248c81c21a0fe825089311b17f2c302eea614a2
Jose Fonseca <jose.r.fonseca@gmail.com>
2019-04-07 10:42:45 +0900

    Bump to version 1.1.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='xdot',
-    version='1.0',
+    version='1.1',
     author='Jose Fonseca',
     author_email='jose.r.fonseca@gmail.com',
     url='https://github.com/jrfonseca/xdot.py',
