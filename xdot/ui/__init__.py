908af8055e6623ff54ac3e435a6fe5843e160c00
Jose Fonseca <jose.r.fonseca@gmail.com>
2021-01-10 10:31:51 +0000

    Provide more helpful error message when gi import fails.
    It's a common problem for those installing xdot.py from PyPI, as it's
    not available there, hence we can't depend upon it.
    
    See also:
    - https://github.com/jrfonseca/xdot.py/issues/88
    - https://github.com/jrfonseca/xdot.py/issues/42

--- xdot/ui/__init__.py
+++ xdot/ui/__init__.py
@@ -1,3 +1,11 @@
 __all__ = ['actions', 'animation', 'colors', 'elements', 'pen', 'window']
 
+import sys
+
+try:
+    import gi
+except ImportError:
+    sys.stderr.write('error: PyGObject bindings for GTK3 not found (https://git.io/JLjeE)\n')
+    sys.exit(1)
+
 from .window import DotWidget, DotWindow
