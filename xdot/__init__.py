e5563ab3b92acb022f64b494c73d78c23ef51ded
Jose Fonseca <jose.r.fonseca@gmail.com>
2022-09-14 09:33:19 +0100

    Leverage PEP 562 for backwards compatibility.
    As suggested https://github.com/jrfonseca/xdot.py/issues/108#issuecomment-1245552802

--- xdot/__init__.py
+++ xdot/__init__.py
@@ -19,6 +19,29 @@
 
 import sys
 assert sys.version_info.major >= 3
-del sys
+
+import importlib
+import warnings
+
+
+__all__ = ['dot', 'ui']
+
 
 __author__ = "Jose Fonseca et al"
+
+
+# Leverage PEP 562 to maintain backwards compatibility while still
+# allowing xdot.dot to be used headlessly.
+if sys.version_info >= (3, 7):
+    def __getattr__(name):
+        if name in ('dot', 'ui'):
+            return importlib.import_module("." + name, __name__)
+        if name in ('DotWidget', 'DotWindow'):
+            warnings.warn(f"w xdot.{name} is deprecated, use xdot.ui.{name} instead", UserWarning, stacklevel=2)
+            from . import ui
+            return getattr(ui, name)
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+else:
+    from . import dot
+    from . import ui
+    from .ui import DotWidget, DotWindow
